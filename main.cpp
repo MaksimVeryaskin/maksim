da19e903092155c40df891d9eb8391c1987902cc
MaksimVeryaskin <159907302+MaksimVeryaskin@users.noreply.github.com>
2024-02-13 15:31:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <cassert>
+int Sum(int a, int b)
+{
+  return a+b;
+}
+int main()
+{
+  cassert(Sum(5, 5 ) == 10);
+  std::cout<<HelloWorld!";
+  return 0;
+}
 
-  int main()
-  {
-    std::cout<<"HelloWorld!";
-    return 0;
-    }
   
